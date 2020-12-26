b975d3212f42013c7f16fcf6b17ca2a7af405355
Dylan Anthony <contact@dylananthony.com>
2020-12-26 15:29:03 -0700

    Update TailWind purge setting to eliminate warning. Add some JetBrains files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
     extend: {
