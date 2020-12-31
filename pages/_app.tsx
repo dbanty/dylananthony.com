bed7b2f59ecacc5793f0e4184aa2090771a2086c
Dylan Anthony <contact@dylananthony.com>
2020-12-30 21:14:16 -0700

    feat(style): Added syntax highlighting (#12)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/index.css";
+import "prismjs/themes/prism-tomorrow.css";
 import { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
