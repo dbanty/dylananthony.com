6719b9831ba04786e0a619d256322cfe9efc5288
Dylan Anthony <contact@dylananthony.com>
2021-02-27 18:13:53 -0700

    fix(layout): Improve spacing of sponsor card on mobile.

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,13 @@ export default function Footer(): JSX.Element {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-16 md:pt-16 md:pb-0 flex flex-col items-center">
           <iframe
             src="https://github.com/sponsors/dbanty/card"
             title="Sponsor dbanty"
             height="225"
-            width="600"
-            style={{ border: 0 }}
+            width="100%"
+            style={{ border: 0, maxWidth: 600 }}
           />
         </div>
       </Container>
