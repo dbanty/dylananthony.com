ccceb27c612018b369442000c958c31cef02b9ce
Dylan Anthony <contact@dylananthony.com>
2021-04-11 18:08:59 -0600

    feat(social): Links to social in footer and a new title on the main page

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,14 +4,35 @@ export default function Footer(): JSX.Element {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-16 md:pt-16 md:pb-0 flex flex-col items-center">
+        <div className="py-16 md:pt-16 md:pb-0 grid grid-cols-1 md:grid-cols-3">
           <iframe
+            className="md:col-span-2"
             src="https://github.com/sponsors/dbanty/card"
             title="Sponsor dbanty"
             height="225"
             width="100%"
             style={{ border: 0, maxWidth: 600 }}
           />
+          <div className="flex flex-col text-left items-center">
+            <h3 className="text-xl font-bold">Follow Me</h3>
+            <ul className="underline">
+              <li>
+                <a href="https://twitter.com/TBDylan">Twitter</a>
+              </li>
+              <li>
+                <a href="https://github.com/dbanty">GitHub</a>
+              </li>
+              <li>
+                <a href="https://dbanty.medium.com">Medium</a>
+              </li>
+              <li>
+                <a href="https://dev.to/dbanty">Dev.to</a>
+              </li>
+              <li>
+                <a href="https://dylananthony.substack.com">Substack </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </Container>
     </footer>
--- components/intro.tsx
+++ components/intro.tsx
@@ -2,10 +2,10 @@ export default function Intro(): JSX.Element {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        A Blog
+        Dylan&apos;s Dev Digest
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        About producing quality software with minimal frustration.
+        Producing quality software with minimal frustration.
       </h2>
     </section>
   );
