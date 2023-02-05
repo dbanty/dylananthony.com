890c4a57c04f8a5e0d3f7d92de3cedcede49108f
Dylan Anthony <dbanty@users.noreply.github.com>
2023-02-05 16:11:12 -0700

    feat: Colorize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,32 @@
 module.exports = {
     content: ["./templates/**/*.html"],
+    safelist: [
+        // Zinc tags
+        "bg-zinc-200",
+        "hover:bg-zinc-300",
+        "dark:bg-zinc-700",
+        "dark:hover:bg-zinc-600",
+        // Orange tags
+        "bg-orange-200",
+        "hover:bg-orange-300",
+        "dark:bg-orange-700",
+        "dark:hover:bg-orange-600",
+        // Fuchsia tags
+        "bg-fuchsia-200",
+        "hover:bg-fuchsia-300",
+        "dark:bg-fuchsia-700",
+        "dark:hover:bg-fuchsia-600",
+        // Teal tags
+        "bg-teal-200",
+        "hover:bg-teal-300",
+        "dark:bg-teal-700",
+        "dark:hover:bg-teal-600",
+        // Indigo tags
+        "bg-indigo-200",
+        "hover:bg-indigo-300",
+        "dark:bg-indigo-700",
+        "dark:hover:bg-indigo-600",
+    ],
     theme: {
         extend: {
             typography: {
