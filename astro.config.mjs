fe5e95b691c42fc25a9b4678659d7f55d74dc082
Dylan Anthony <dbanty@users.noreply.github.com>
2024-03-03 16:24:22 -0700

    Custom code block colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://next.dylananthony.com",  // TODO: Point at primary domain
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind({applyBaseStyles: false,})],
   redirects: {
     "/": "/blog",
   },
+  markdown: {
+    syntaxHighlight: "prism"
+  }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  // TODO: Code block styles
   theme: {
     extend: {},
   },
